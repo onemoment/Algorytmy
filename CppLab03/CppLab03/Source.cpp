3f69892e3766d468eab3d875f439426426ac8cfa
student <student>
2015-11-21 14:24:02 +0100

    working

--- CppLab03/CppLab03/Source.cpp
+++ CppLab03/CppLab03/Source.cpp
@@ -5,10 +5,17 @@
 
 using namespace std;
 
+enum Tryb{
+	trybEdycja,
+	trybDodaj,
+	trybUsun
+};
+
 void PokazTeleadres(Ksiazka_teleadresowa T);
 void PokazKsiazke(Ksiazka_teleadresowa *K, int Rozmiar);
 
-void DodajElement(Ksiazka_teleadresowa *K, int Rozmiar);
+void ZmienElement(Ksiazka_teleadresowa *K, int Index, Tryb tryb, int dane);
+void RejestracjaObiektu(Ksiazka_teleadresowa *K, int Rozmiar, Tryb tryb);
 
 int main() {
 
@@ -20,6 +27,8 @@ int main() {
 		cout << "MENU\n";
 		cout << "1. Wyswietl ksiazke adresowa\n";
 		cout << "2. Dodaj element\n";
+		cout << "3. Usun element\n";
+		cout << "4. Edytuj element\n";
 		cout << "0. Wyjscie\n";
 
 		cin >> a;
@@ -29,8 +38,15 @@ int main() {
 		case 1: PokazKsiazke(tab_el, 3);
 			break;
 
-		case 2: DodajElement(tab_el, 3);
+		case 2: RejestracjaObiektu(tab_el, 3, trybDodaj);
 			break;
+
+		case 3: RejestracjaObiektu(tab_el, 3, trybUsun);
+			break;
+
+		case 4: RejestracjaObiektu(tab_el, 3, trybEdycja);
+			break;
+
 		default:
 			cout << "Bledny wybor\n";
 		}
@@ -56,32 +72,77 @@ void PokazKsiazke(Ksiazka_teleadresowa *K, int Rozmiar){
 	}
 }
 
-void DodajElement(Ksiazka_teleadresowa *K, int Rozmiar){
-	cout << "Wskaz, w ktorym miejscu chcesz dodac kontakt, i = ";
-	int index;
-
-	string aString;
+void ZmienElement(Ksiazka_teleadresowa *K, int Index, Tryb tryb, int dane){
+	string aString, aDodatekA, aDodatekB, aDodatekC;
 	int aDzien, aMiesiac, aRok;
 
-	cin >> index;
-
-	if (index > Rozmiar || index < 1){
-		cout << "No chyba nie.. Brak takiego elementu.";
+	if (tryb == trybUsun){
+		K[Index] = Ksiazka_teleadresowa();
 	}
 	else {
+		bool czyEdycja = tryb == trybEdycja;
 
+		if (czyEdycja){
+			aDodatekA = "Nowe ";
+			aDodatekB = "Nowa ";
+			aDodatekC = "Nowy ";
+		}
+		else{
+			aDodatekA = "";
+			aDodatekB = "";
+			aDodatekC = "";
+		}
+
+		if (!czyEdycja || dane == 1) { cout << aDodatekA << "Imie: "; cin >> aString; K[Index].set_imie(aString); }
+		if (!czyEdycja || dane == 2) { cout << aDodatekA << "Nazwisko: "; cin >> aString; K[Index].set_nazwisko(aString); }
+		if (!czyEdycja || dane == 3){
+			cout << aDodatekB << "Data urodzenia:" << endl;
+			cout << aDodatekC << "Dzien: "; cin >> aDzien;
+			cout << aDodatekC << "Miesiac: "; cin >> aMiesiac;
+			cout << aDodatekC << "Rok: "; cin >> aRok;
+			K[Index].set_data_ur(aDzien, aMiesiac, aRok);
+		}
+	}
+}
+
+void RejestracjaObiektu(Ksiazka_teleadresowa *K, int Rozmiar, Tryb tryb){
+
+	int index;
+	int wybor;
+
+	switch (tryb)
+	{
+	case trybDodaj:
+		cout << "Wskaz, w ktorym miejscu chcesz dodac kontakt, i = ";
+		cin >> index;
+		wybor = 0;
+		break;
+
+	case trybEdycja:
+		cout << "Wskaz, w ktorym miejscu chcesz dodac kontakt, i = ";
+		cin >> index;
+		cout << "Podaj, jakie dane chcesz edytowac:\n1. Imie\n2. Nazwisko\n3. DataUrodzenia\n";
+		cin >> wybor;
+		break;
+
+	case trybUsun:
+		cout << "Wskaz, ktory kontakt chcesz usunac, i = ";
+		cin >> index;
+		wybor = 0;
+		break;
+
+	default:
+		break;
+	}
+
+
+	
+	if (index > Rozmiar || index < 1){
+		cout << "Brak takiego elementu.";
+	}
+	else {
 		index--;
-
-		cout << "Imie: "; cin >> aString; K[index].set_imie(aString);
-		cout << "Nazwisko: "; cin >> aString; K[index].set_nazwisko(aString);
-
-		cout << "Data urodzenia:" << endl;
-
-		cout << "Dzien: "; cin >> aDzien;
-		cout << "Miesiac: "; cin >> aMiesiac; 
-		cout << "Rok: "; cin >> aRok; 
-
-		K[index].set_data_ur(aDzien, aMiesiac, aRok);
+		ZmienElement(K, index, tryb, wybor);
 	}
 }
 /*
