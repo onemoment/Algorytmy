1d3458a6dd431a2a6d627972911a2115baf24752
student <student>
2015-12-06 12:14:41 +0100

    heap

---
+++ HeapSort/HeapSort/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int wektor[] = { 12, 20, 15, 29, 23, 17, 22, 35, 40, 26, 51, 19 };
+
+	system("PAUSE");
+	return 0;
+}
+
+/*
+korzeñ= 1
+wartoœæ(i)  =x[i]
+lewysyn(i)  = 2*i
+prawysyn(i) = 2*i+1
+ojciec(i)   =i div 2
+*/
